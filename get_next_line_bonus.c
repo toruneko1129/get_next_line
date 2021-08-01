92262cbed119c3eff9b490ed4ab573f02fcde170
Hayato Kawakita <hkawakit@c1r3s6.42tokyo.jp>
2021-08-01 22:19:26 +0900

    updated

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hkawakit <hkawakit@student.42tokyo.j>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 21:09:11 by hkawakit          #+#    #+#             */
-/*   Updated: 2021/08/01 21:43:42 by hkawakit         ###   ########.fr       */
+/*   Updated: 2021/08/01 22:19:17 by hkawakit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,47 +32,46 @@ static int	load_state(int fd, t_map **map, t_map **res)
 	return (SUCCESS);
 }
 
-static int	get_text_from_file(char *buf, t_map **res)
+static int	get_text_from_file(t_map *res, char *buf)
 {
 	t_list	*last;
 	char	*endl;
 	ssize_t	cnt;
 
-	last = (*res)->lst;
+	last = res->lst;
 	endl = ft_strchr(last, '\n');
 	cnt = 1;
-	while (cnt && endl == NULL)
+	while (endl == NULL && cnt)
 	{
-		cnt = read((*res)->fd, buf, (size_t)BUFFER_SIZE);
+		cnt = read(res->fd, buf, (size_t)BUFFER_SIZE);
 		if (cnt == -1)
 			return (FAILED);
 		*(buf + cnt) = '\0';
 		if (ft_lstadd_back(&last, buf, cnt))
 			return (FAILED);
-		if ((*res)->lst == NULL)
-			(*res)->lst = last;
-		(*res)->tlen += cnt;
+		if (res->lst == NULL)
+			res->lst = last;
+		res->tlen += cnt;
 		endl = ft_strchr(last, '\n');
 	}
 	if (endl != NULL)
-		(*res)->nlen = ft_strchr(last, '\0') - endl - 1;
-	t_list	*tmp = (*res)->lst;
+		res->nlen = ft_strchr(last, '\0') - endl - 1;
 	return (SUCCESS);
 }
 
 char	*get_next_line(int fd)
 {
 	static t_map	*map = NULL;
-	char			*buf;
 	t_map			*res;
+	char			*buf;
 
-	if (fd < 0 || (ssize_t)BUFFER_SIZE <= 0)
+	res = NULL;
+	if (fd < 0 || (ssize_t)BUFFER_SIZE <= 0 || load_state(fd, &map, &res))
 		return (NULL);
 	buf = (char *)malloc(((size_t)BUFFER_SIZE + 1) * sizeof(char));
 	if (buf == NULL)
 		return (NULL);
-	res = NULL;
-	if (load_state(fd, &map, &res) || get_text_from_file(buf, &res))
+	if (get_text_from_file(res, buf))
 	{
 		ft_mapdelone(&map, &res);
 		free(buf);
